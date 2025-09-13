5458f2284800d197749effdc31e26d85c04ad50d
BunnyCoding <z.yong@poomjit.com>
2025-09-13 08:28:40 +0800

    Add files via upload

--- src/models/UserSettings.ts
+++ src/models/UserSettings.ts
@@ -2,21 +2,20 @@
 import { Schema, models, model, Model } from "mongoose";
 
 export interface IUserSettings {
-  pilotId: string;      // JAL1234 (uppercase)
+  apiKey: string;     // ใช้ค่านี้เป็นตัวอ้างอิงหลัก
   hoppieId?: string;
   simbriefId?: string;
 }
 
 const UserSettingsSchema = new Schema<IUserSettings>(
   {
-    pilotId: { type: String, required: true, unique: true, index: true },
+    apiKey: { type: String, required: true, unique: true, index: true },
     hoppieId: { type: String, default: "" },
     simbriefId: { type: String, default: "" },
   },
   { timestamps: true }
 );
 
-// ใส่ชนิดให้โมเดลแบบชัด ๆ
 export const UserSettings: Model<IUserSettings> =
   (models.UserSettings as Model<IUserSettings>) ||
   model<IUserSettings>("UserSettings", UserSettingsSchema);
