b98001aad44e08a8bb7e7482e4817735bf5ac72d
BunnyCoding <z.yong@poomjit.com>
2025-09-13 15:05:35 +0800

    Delete src/app directory

--- src/app/providers.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-import { CookiesProvider } from 'react-cookie';
-
-export default function Providers({ children }: { children: React.ReactNode }) {
-  return <CookiesProvider>{children}</CookiesProvider>;
-}
